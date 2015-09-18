9b07d6db10b3ba6a07bb09023368f05f30e2db61
Remmelt Pit <remmelt.pit@container-solutions.com>
2015-09-18 17:54:28 +0200

    Test setup works

--- logstash.sh
+++ logstash.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 docker build -t containersol/logstash .
 
+# Note that you have to have a --name because you don't want multiple
+# logstashes running because that will lead to double logs.
 docker run \
+  --name logstash \
   -v "$PWD":/config-dir \
-  -it --rm \
+  -v "$PWD/logs":/logs \
+  -d \
   -e SLACK_HOOK_URL=$SLACK_HOOK_URL \
   containersol/logstash
+
+# A test container that logs to a log file
+docker run \
+  -v "$PWD":/mnt \
+  --rm \
+  debian:jessie \
+  /mnt/test.sh
---
+++ test.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+echo "$(hostname) says: $(date)" >> /mnt/logs/test.log
