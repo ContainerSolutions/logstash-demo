6940fc00c82530e9373a8291b5e5a69822468f5e
Remmelt Pit <remmelt.pit@container-solutions.com>
2015-09-18 17:01:26 +0200

    Added check for slack hook url env var

--- boot.sh
+++ boot.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
+if [ -z $SLACK_HOOK_URL ]; then
+  echo "No SLACK_HOOK_URL set. Don't forget to export the env var!"
+  exit 1
+fi
+
 logstash -e "$(sed -e "s,%%SLACK_HOOK_URL%%,$SLACK_HOOK_URL,g" /config-dir/logstash.conf)"
